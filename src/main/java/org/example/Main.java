ed2652a7a9cf397be2e6062150ed055bd52133fb
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-03-05 16:06:53 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,39 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> result;
+        System.out.println(result = getRandomList(25, 1, 100));
+        System.out.println(bubbleSort(result));
+    }
+
+    public static ArrayList<Integer> getRandomList(int n, int min, int max) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            list.add(random.nextInt((max - min) + 1) + min);
+        }
+
+        return list;
+    }
+
+    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> list) {
+        int count = 1;
+        int temp;
+        while (count > 0) {
+            count = 0;
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i + 1) < list.get(i)) {
+                    temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                    count++;
+                }
+            }
+        }
+        return list;
     }
 }
--- src/test/java/BubbleSortTest.java
+++ src/test/java/BubbleSortTest.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class BubbleSortTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.example.Main.bubbleSort;
+
+
+public class BubbleSortTest {
+    @Test
+    void testFun() {
+        ArrayList<Integer> testList = new ArrayList<>();
+        testList.add(3);
+        testList.add(2);
+        testList.add(1);
+        ArrayList<Integer> actual = bubbleSort(testList);
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
